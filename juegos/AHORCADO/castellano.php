b5ce2f6fb966682891fcafedcfbc9a295d12b8fe
Luis <luis_07_95@hotmail.com>
2019-04-17 12:33:46 +0200

    all data erased

--- juegos/AHORCADO/castellano.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('TITULO','AHORCADO');
-define('PALABRA','PALABRA');
-define('INTENTOS','INTENTOS');
-define('ENVIAR','ENVIAR');
-define('','');
-?>
--- juegos/AHORCADO/ingles.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('TITULO','HANG MAN');
-define('PALABRA','WORD');
-define('INTENTOS','ATTEMPTS');
-define('ENVIAR','SEND');
-
-?>
